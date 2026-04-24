8e5c6f656b19469566f5d6b51a37695da4ab9b67
Hugo W.L. ter Doest <hwl.ter.doest@gmail.com>
2026-04-24 21:17:47 +0200

    Remove tmp js file

--- tmp-check-changelog.js
+++
@@ -1,3 +0,0 @@
-const { checkChangelog } = require('./src/lib/checkers/changelog');
-const result = checkChangelog(['README.md', 'CHANGELOG.md', 'src/index.ts']);
-console.log(JSON.stringify(result, null, 2));
